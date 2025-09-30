e1475a1f1fbb844db3a121e383f3866269fd376b
Jose19283284729387 <129287691+Jose19283284729387@users.noreply.github.com>
2025-09-29 21:14:15 -0300

    Add files via upload

---
+++ CEN-main/examen_cen/Oferta.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cursado</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navegador">
+    <span><img src="" alt=""> CEN</span>
+    <button>Información</button>
+    <button>Campus Virtual</button>
+    <button>Instagram</button>
+    <button>Facebook</button></nav>
+
+    <div class="titulo"><h1>Años a cursar</h1></div>
+    
+    <div class="container">
+        <div><img src="" alt=""> <div><p>Primer año</p></div></div>
+        <div><img src="" alt=""> <div><p>Segundo año</p></div></div>
+        <div><img src="" alt=""> <div><p>Tercer año</p></div></div>
+        <div><img src="" alt=""> <div><p>Cuarto año</p></div></div>
+        <div><img src="" alt=""> <div><p>Quinto año</p></div></div>
+        <div><img src="" alt=""> <div><p>Sexto año</p></div></div>
+    </div>
+
+
+    <footer><p>CEN © CENTRO EDUCATIVO NONOGASTA</p></footer>
+</body>
+</html>
