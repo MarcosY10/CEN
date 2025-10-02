43fb5189d29aae0e4719d8190a21cacdc299da51
brunon510 <bruno838bss@gmail.com>
2025-10-02 16:51:08 -0300

    Add files via upload

---
+++ examen_cen/Galeria.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav >
+    <img onclick="window.location.href='Index.php'" src="media/LOGO1.png" alt="Logo" style="width:7%;">
+    <span class="upt"><img src="" alt=""> CEN</span>
+
+    </nav>
+    
+
+    <footer><p>CEN © CENTRO EDUCATIVO NONOGASTA</p></footer>
+</body>
+</html>
